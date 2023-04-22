777ad6dec0b70d0540fc607741bd39a121941b6b
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-04-22 13:35:51 +0530

    womensection

--- lib/ui/womensection.dart
+++ lib/ui/womensection.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshop_provider/shared/appstyle.dart';
+import 'package:onlineshop_provider/shared/latestshoes.dart';
+import 'package:onlineshop_provider/shared/shoecard.dart';
 
 class WomenSection extends StatelessWidget {
   const WomenSection({super.key});
@@ -8,19 +10,63 @@ class WomenSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFAFAFA),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text(
-                  "Women's Shoes",
-                  style: appstyle(24, Colors.black, FontWeight.bold),
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "Women's Shoes",
+                style: appstyle(24, Colors.black, FontWeight.bold),
               ),
-            ],
-          ),
+            ),
+            Container(
+              color: const Color(0xFFFAFAFA),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.34,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return const ShoeCard();
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Latest Shoes",
+                    style: appstyle(18, Colors.black, FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Show all ",
+                        style: appstyle(18, Colors.black, FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 18,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return const LatestShoeCard();
+                  }),
+            ),
+          ],
         ),
       ),
     );
