b30a99d9959cce40ee2b28ae08bb7c6125a9f591
kaushal1717 <kpadaliya1717@gmail.com>
2023-04-28 15:10:12 +0530

    commit

--- lib/ui/onboardingpage.dart
+++ lib/ui/onboardingpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshop_provider/ui/obpages.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -10,18 +11,49 @@ class OnboardingPage extends StatefulWidget {
 
 class _OnboardingPageState extends State<OnboardingPage> {
   final _pageController = PageController();
+  static const _duration = Duration(milliseconds: 500);
+  static const _curve = Curves.ease;
+  final _arrowColor = Color(0xffff8282).withOpacity(0.8);
+  int _activePage = 0;
+
+  final List<Widget> _pages = [
+    const PageOne(),
+    const PageTwo(),
+    const PageThree()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final List<Widget> _pages = [PageOne(), PageTwo(), PageThree()];
     return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        PageView.builder(
-            itemCount: _pages.length,
-            controller: _pageController,
-            itemBuilder: ((context, index) {
-              return _pages[index % _pages.length];
-            }))
-      ]),
+      body: IconTheme(
+        data: IconThemeData(color: _arrowColor),
+        child: Stack(
+          children: [
+            PageView.builder(
+                physics: AlwaysScrollableScrollPhysics(),
+                controller: _pageController,
+                itemBuilder: (BuildContext context, int index) {
+                  return _pages[index % _pages.length];
+                }),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.grey.withOpacity(0.5),
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                      child: SmoothPageIndicator(
+                    controller: _pageController,
+                    count: _pages.length,
+                    effect: ExpandingDotsEffect(
+                        dotColor: Colors.white,
+                        activeDotColor: Color(0xffff8282)),
+                  )),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
